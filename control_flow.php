73ee0989fdf7548e2c7d71bc9dd157f550eb885c
Levin <levindw05@gmail.com>
2024-10-04 17:02:00 +0800

    Update control_flow.php

--- control_flow.php
+++ control_flow.php
@@ -1,11 +1,13 @@
 <?php
 include 'variables.php';
+
+$student = new Student("Levin", "2005-06-22", "male", 3.5, true);
+$age = $student->ageYears;
+
 echo "<br>";
-    if ($age >= 18) {
-        echo "You are an adult";
-    } elseif ($age < 18 && $age > 0) {
-        echo "You are a minor";
-    } else {
-        echo "Age must be above 0";
-    }
+if ($age < 0) {
+    echo "Age must be above 0";
+} else {
+    echo ($age >= 18) ? "You are an adult" : "You are a minor";
+}
 ?>
