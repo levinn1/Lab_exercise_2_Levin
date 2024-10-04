d663c100bb258be70d735d8158a6840f7460aaad
Levin <levindw05@gmail.com>
2024-10-04 17:02:45 +0800

    Update variables.php

--- variables.php
+++ variables.php
@@ -1,40 +1,36 @@
 <?php
 class Student {
-    public $name;     
-    public $year;
-    public $month;
-    public $day;    
-    public $sex;       
-    public $gpa;     
+    public $name;
+    public $ageYears;
+    public $ageMonths;
+    public $ageDays;
+    public $sex;
+    public $gpa;
     public $isStudent;
 
-    function __construct($name, $date, $sex, $gpa, $isStudent) {
+    public function __construct($name, $birthDate, $sex, $gpa, $isStudent) {
         $this->name = $name;
-        $this->Age($date);
+
+        // Calculate age details
+        $birthDate = new DateTime($birthDate);
+        $now = new DateTime();
+        $ageDifference = $now->diff($birthDate);
+
+        $this->ageYears = $ageDifference->y;
+        $this->ageMonths = $ageDifference->m;
+        $this->ageDays = $ageDifference->d;
+
         $this->sex = $sex;
         $this->gpa = $gpa;
         $this->isStudent = $isStudent;
     }
 
-    function Age($date) {
-        $birth = new DateTime($date);
-        $datenow = new DateTime();    
-        $age = $datenow->diff($birth); 
-        $this->year = $age->y;
-        $this->month = $age->m; 
-        $this->day = $age->d;  
-    }
-
-    public function getAge() {
-        return $this->year; 
-    }
-
-    public function message() {
-        echo "Student name is " . $this->name . ", Age is " . $this->year . " years, " . $this->month . " months, " . $this->day . " days, Sex is " . $this->sex . ",  is " . $this->gpa . ", Is a student? " . ($this->isStudent ? 'Yes' : 'No');
+    public function displayInfo() {
+        echo "Name: $this->name\n";
+        echo "Age: $this->ageYears years, $this->ageMonths months, and $this->ageDays days\n";
+        echo "Sex: $this->sex\n";
+        echo "GPA: $this->gpa\n";
+        echo "Is Student: " . ($this->isStudent ? 'Yes' : 'No') . "\n";
     }
 }
-
-$student = new Student("Levin", "2005-06-22", "Male", 4.9, true);
-$age = $student->getAge();
-echo $student->message();
 ?>
